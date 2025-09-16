133a60d54cad55d314b15aac900c68ff16c669aa
Reem Almutairi <reem.naw@gmail.com>
2025-09-16 21:37:19 +0300

    update background color and fonts weights

--- lib/a_presentation/a_shared/text_styles.dart
+++ lib/a_presentation/a_shared/text_styles.dart
@@ -8,51 +8,51 @@ const headerStyle = TextStyle(
     textBaseline: TextBaseline.alphabetic);
 const subHeaderStyle = TextStyle(
   fontSize: 16.0,
-  fontWeight: FontWeight.w700,
+  fontWeight: FontWeight.w500,
   fontFamily: 'the_year_of_handicrafts',
 );
 const summaryTextStyle = TextStyle(
   fontSize: 16,
-  fontWeight: FontWeight.w600,
+  fontWeight: FontWeight.w400,
   fontFamily: 'the_year_of_handicrafts',
 );
 const summaryNumberTextStyle = TextStyle(
   fontSize: 28,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.w600,
   fontFamily: 'the_year_of_handicrafts',
 );
 
 // Text Styles
 final kTitleLarge = TextStyle(
   fontSize: 28.0,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.bold,
   color: kBlacksColor[400],
   fontFamily: 'the_year_of_handicrafts',
 );
 const kHeadlineSmall = TextStyle(
   fontSize: 22.0,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.bold,
   color: kPrimaryLabelColor,
   fontFamily: 'the_year_of_handicrafts',
 );
 
 const kHeadlineMedium = TextStyle(
   fontSize: 18.0,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.bold,
   color: kPrimaryLabelColor,
   fontFamily: 'the_year_of_handicrafts',
 );
 
 const kHeadLineStyle = TextStyle(
   fontSize: 17.0,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.bold,
   color: kPrimaryLabelColor,
   fontFamily: 'the_year_of_handicrafts',
 );
 
 const kBodyLarge = TextStyle(
   fontSize: 17.0,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.bold,
   color: kBlacksColor,
   fontFamily: 'the_year_of_handicrafts',
   textBaseline: TextBaseline.ideographic,
@@ -60,21 +60,21 @@ const kBodyLarge = TextStyle(
 
 const kCalloutStyle = TextStyle(
   fontSize: 16.0,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.bold,
   color: kBlacksColor,
   fontFamily: 'the_year_of_handicrafts',
 );
 
 const kBodyMedium = TextStyle(
   fontSize: 15.0,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.w600,
   color: kPrimaryLabelColor,
   fontFamily: 'the_year_of_handicrafts',
 );
 
 const kFootnoteStyle = TextStyle(
   fontSize: 13.0,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.w600,
   color: kBlacksColor,
   fontFamily: 'the_year_of_handicrafts',
 );
@@ -82,14 +82,14 @@ const kFootnoteStyle = TextStyle(
 const kCaption1Style = TextStyle(
   fontSize: 12.0,
   decoration: TextDecoration.none,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.bold,
   color: kPrimaryLabelColor,
   fontFamily: 'the_year_of_handicrafts',
 );
 
 const kCaption2Style = TextStyle(
   fontSize: 11.0,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.bold,
   color: kPrimaryLabelColor,
   fontFamily: 'the_year_of_handicrafts',
 );
@@ -97,6 +97,5 @@ const kCaption2Style = TextStyle(
 TextStyle kLoginInputTextStyle = const TextStyle(
   fontSize: 16.0,
   color: kSecondaryLabelColor,
-  fontWeight: FontWeight.w600,
   fontFamily: 'the_year_of_handicrafts',
 );
