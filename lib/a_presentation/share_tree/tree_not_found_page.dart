0913c7bb7f615f58a3201284ca0edfd326f088a8
ReemNawaf <reem.naw@gmail.com>
2026-01-02 12:05:16 +0300

    update share settings & not found tree page

--- lib/a_presentation/share_tree/tree_not_found_page.dart
+++ lib/a_presentation/share_tree/tree_not_found_page.dart
@@ -1,6 +1,7 @@
 import 'package:asl/a_presentation/a_shared/app_colors.dart';
 import 'package:asl/a_presentation/a_shared/constants.dart';
 import 'package:asl/a_presentation/a_shared/text_styles.dart';
+import 'package:asl/a_presentation/core/widgets/logo_slogan_wdg.dart';
 import 'package:asl/a_presentation/routes/app_router.dart';
 import 'package:asl/a_presentation/core/widgets/info_container.dart';
 import 'package:asl/localization/localization_constants.dart';
@@ -14,38 +15,55 @@ class TreeNotFoundPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: kWhitesColor[600],
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      child: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            '${getTr(context, 'sorry_there_is_no_tree_under_this_link')} 🌴',
-            style: kHeadlineSmall.copyWith(fontSize: 24),
-          ),
-          kVSpacer5,
-          Text(
-            getTr(context, 'the_link_is_incorrect_or_tree_is_not_available')!,
-            style: kBodyLarge.copyWith(wordSpacing: 2.0, height: 1.8),
-          ),
-          kVSpacer10,
-          InfoContainer(
-              info: getTr(context, 'ask_tree_editor_to_send_the_right_link')!),
-          kVSpacer20,
-          GestureDetector(
-            onTap: () => context.router.push(const AuthRoute()),
-            child: Text(
-              getTr(context, 'go_to_home_page')!,
-              style: kHeadlineSmall.copyWith(
-                  fontSize: 16,
-                  decoration: TextDecoration.underline,
-                  color: kRootColors),
+    return Scaffold(
+      backgroundColor: kWhitesColor[600],
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            logo(),
+            kVSpacer10,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100.0),
+              child: Divider(
+                color: kRootColors[600],
+                thickness: 2,
+              ),
             ),
-          )
-        ],
-      )),
+            kVSpacer20,
+            Text(
+              '${getTr(context, 'sorry_there_is_no_tree_under_this_link')!} 🌴✨',
+              style: kHeadlineSmall.copyWith(fontSize: 24),
+            ),
+            kVSpacer10,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 70.0),
+              child: Text(
+                getTr(
+                    context, 'the_link_is_incorrect_or_tree_is_not_available')!,
+                style: kBodyLarge.copyWith(wordSpacing: 2.0, height: 1.8),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            kVSpacer20,
+            InfoContainer(
+              info: getTr(context, 'ask_tree_editor_to_send_the_right_link')!,
+            ),
+            kVSpacer20,
+            GestureDetector(
+              onTap: () => context.router.push(const AuthRoute()),
+              child: Text(
+                getTr(context, 'go_to_home_page')!,
+                style: kHeadlineSmall.copyWith(
+                    fontSize: 16,
+                    decoration: TextDecoration.underline,
+                    color: kRootColors),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
