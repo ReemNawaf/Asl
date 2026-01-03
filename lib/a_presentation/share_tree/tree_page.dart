618154b8a57e61da4b28e394c83033c794847a95
ReemNawaf <reem.naw@gmail.com>
2026-01-03 22:05:37 +0300

    update tree_page with hide sidebar + search

--- lib/a_presentation/share_tree/tree_page.dart
+++ lib/a_presentation/share_tree/tree_page.dart
@@ -1,7 +1,7 @@
 import 'package:asl/a_presentation/a_shared/app_colors.dart';
-import 'package:asl/a_presentation/a_shared/box_dec.dart';
 import 'package:asl/a_presentation/a_shared/constants.dart';
 import 'package:asl/a_presentation/core/widgets/des_loading_wdg.dart';
+import 'package:asl/a_presentation/home/tree_search_bar.dart';
 import 'package:asl/a_presentation/home/widgets/layer_wdg.dart';
 import 'package:asl/a_presentation/home/widgets/settings_btn.dart';
 import 'package:asl/a_presentation/home/widgets/tree_list.dart';
@@ -70,76 +70,116 @@ class TreePage extends StatelessWidget {
 
                 return size.height < MIM_HEIGHT || size.width < MIM_WIDTH
                     ? const SmallScreenPage()
-                    : Row(
-                        children: [
-                          Container(
-                            color: kWhitesColor[600],
-                            width: size.width * 0.18,
-                            height: size.height,
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 20, horizontal: 14.0),
-                            child: Column(
-                              children: [
-                                if (state.trees.isNotEmpty)
-                                  ListTreeItem(
-                                    id: state.trees.first.treeId,
-                                    treeName:
-                                        state.trees.first.treeName.getOrCrash(),
-                                    rootLetter: state.trees.first.fullName
-                                        .getOrCrash()
-                                        .substring(0, 1),
-                                  )
-                                else
-                                  const SizedBox(),
-                                const Spacer(),
-                                const LayersWidget(),
-                                const Spacer(),
-                                const SettingsButton(),
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            width: size.width * 0.82,
-                            child: Stack(
-                              children: [
-                                SizedBox(
-                                  width: size.width * 0.82,
-                                  child: const InteractiveView(),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 14.0, right: 14.0, top: 14.0),
-                                  child: TextField(
-                                    textAlign: TextAlign.right,
-                                    decoration: kSearchBarInputDecor(context),
-                                  ),
-                                ),
+                    : BlocBuilder<TreeSettingsBloc, TreeSettingsState>(
+                        builder: (context, settingsState) {
+                          return Row(
+                            children: [
+                              if (!settingsState.hideSidbar)
                                 Container(
-                                  width: 150,
-                                  padding: EdgeInsets.only(
-                                      right: 20, top: size.height - 85),
-                                  child: BlocBuilder<TreeSettingsBloc,
-                                      TreeSettingsState>(
-                                    builder: (context, state) {
-                                      return Slider(
-                                        min: MIN_ZOOM,
-                                        max: MAX_ZOOM,
-                                        value: state.zoomScale,
-                                        label:
-                                            "${state.zoomScale.toStringAsFixed(2)}x",
-                                        onChanged: (newScale) {
-                                          context.read<TreeSettingsBloc>().add(
-                                              TreeSettingsEvent.zoomChanged(
-                                                  newScale));
-                                        },
-                                      );
-                                    },
+                                  color: kWhitesColor[600],
+                                  width: size.width * 0.16,
+                                  height: size.height,
+                                  child: Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 20, horizontal: 14.0),
+                                    width: 200.0,
+                                    child: Column(
+                                      children: [
+                                        if (state.trees.isNotEmpty)
+                                          ListTreeItem(
+                                            id: state.trees.first.treeId,
+                                            treeName: state.trees.first.treeName
+                                                .getOrCrash(),
+                                            rootLetter: state
+                                                .trees.first.fullName
+                                                .getOrCrash()
+                                                .substring(0, 1),
+                                          )
+                                        else
+                                          const SizedBox(),
+                                        const Spacer(),
+                                        const LayersWidget(),
+                                        const Spacer(),
+                                        const SettingsButton(),
+                                      ],
+                                    ),
                                   ),
                                 ),
-                              ],
-                            ),
-                          ),
-                        ],
+                              Container(
+                                width: 20,
+                                height: size.height,
+                                color: kWhitesColor[500],
+                                child: RawMaterialButton(
+                                  onPressed: () =>
+                                      context.read<TreeSettingsBloc>().add(
+                                            const TreeSettingsEvent
+                                                .updateHideSideBar(),
+                                          ),
+                                  padding: EdgeInsets.only(
+                                      right: settingsState.hideSidbar ? 0 : 6),
+                                  elevation: 0,
+                                  hoverElevation: 0,
+                                  focusElevation: 0,
+                                  highlightElevation: 0,
+                                  constraints: const BoxConstraints(
+                                      minWidth: 0, minHeight: 0),
+                                  focusColor: Colors.transparent,
+                                  hoverColor: Colors.transparent,
+                                  splashColor: Colors.transparent,
+                                  highlightColor: Colors.transparent,
+                                  child: Icon(
+                                    settingsState.hideSidbar
+                                        ? Icons.arrow_forward_ios
+                                        : Icons.arrow_back_ios,
+                                    color: kRootColors[400],
+                                    weight: 20,
+                                    grade: 25,
+                                    size: 18,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: size.width *
+                                    (settingsState.hideSidbar ? 0.983 : 0.82),
+                                child: Stack(
+                                  children: [
+                                    SizedBox(
+                                      width: size.width *
+                                          (settingsState.hideSidbar
+                                              ? 0.983
+                                              : 0.82),
+                                      child: const InteractiveView(),
+                                    ),
+                                    const TreeSearchBar(),
+                                    Container(
+                                      width: 150,
+                                      padding: EdgeInsets.only(
+                                          right: 20, top: size.height - 85),
+                                      child: BlocBuilder<TreeSettingsBloc,
+                                          TreeSettingsState>(
+                                        builder: (context, state) {
+                                          return Slider(
+                                            min: MIN_ZOOM,
+                                            max: MAX_ZOOM,
+                                            value: state.zoomScale,
+                                            label:
+                                                "${state.zoomScale.toStringAsFixed(2)}x",
+                                            onChanged: (newScale) {
+                                              context
+                                                  .read<TreeSettingsBloc>()
+                                                  .add(TreeSettingsEvent
+                                                      .zoomChanged(newScale));
+                                            },
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          );
+                        },
                       );
               } else {
                 return const TreeNotFoundPage();
