d35c4a6b13a95d02263abb1cad29c2897ef3f9a3
Reem Almutairi <reem.naw@gmail.com>
2026-01-03 21:29:42 +0300

    Revert "fix zoom-in & zoom-out for deploy version"
    This reverts commit 79e9ed90b9e672b1b74745f822dfa59aa82636c4.

--- lib/a_presentation/tree/interactive_viewer.dart
+++ lib/a_presentation/tree/interactive_viewer.dart
@@ -3,7 +3,6 @@ import 'package:asl/a_presentation/tree/tree_view.dart';
 import 'package:asl/b_application/local_tree_bloc/local_tree_bloc.dart';
 import 'package:asl/b_application/tree_bloc/draw_tree/draw_tree_bloc.dart';
 import 'package:asl/b_application/tree_bloc/tree_settings/tree_settings_bloc.dart';
-import 'package:asl/c_domain/local_tree_views/tree_nav_helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -29,10 +28,11 @@ class InteractiveView extends StatelessWidget {
         }
 
         return BlocListener<TreeSettingsBloc, TreeSettingsState>(
-          listener: (context, state) => zoomToScale(
-              viewerKey: context.read<DrawTreeBloc>().state.viewerKey,
-              controller: context.read<DrawTreeBloc>().state.controller,
-              newScale: state.zoomScale),
+          listener: (context, state) => context
+              .read<DrawTreeBloc>()
+              .state
+              .controller
+              .value = Matrix4.identity()..scale(state.zoomScale),
           child: BlocListener<TreeSettingsBloc, TreeSettingsState>(
             listenWhen: (prev, curr) =>
                 prev.numberOfGenerations != curr.numberOfGenerations,
@@ -51,23 +51,17 @@ class InteractiveView extends StatelessWidget {
                     );
               }
             },
-            child: Expanded(
-                child: ClipRect(
-              child: SizedBox.expand(
-                key: context.read<DrawTreeBloc>().state.viewerKey,
-                child: InteractiveViewer(
-                  key: context.read<DrawTreeBloc>().state.viewerKey,
-                  constrained: false,
-                  transformationController:
-                      context.read<DrawTreeBloc>().state.controller,
-                  alignment: Alignment.center,
-                  boundaryMargin: const EdgeInsets.all(5000),
-                  minScale: MIN_ZOOM,
-                  maxScale: MAX_ZOOM,
-                  child: const TreeView(),
-                ),
-              ),
-            )),
+            child: InteractiveViewer(
+              key: context.read<DrawTreeBloc>().state.viewerKey,
+              constrained: false,
+              transformationController:
+                  context.read<DrawTreeBloc>().state.controller,
+              alignment: Alignment.center,
+              boundaryMargin: const EdgeInsets.all(5000),
+              minScale: MIN_ZOOM,
+              maxScale: MAX_ZOOM,
+              child: const TreeView(),
+            ),
           ),
         );
       },
